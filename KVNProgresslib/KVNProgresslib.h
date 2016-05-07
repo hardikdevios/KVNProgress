666147ab3aa983dbdcbcfd5037a58fa3ba9fd1a1
Hardik <hardik.webatease@gmail.com>
2016-05-07 23:43:28 +0530

    - Carthage Support

---
+++ KVNProgresslib/KVNProgresslib.h
@@ -0,0 +1,19 @@
+//
+//  KVNProgresslib.h
+//  KVNProgresslib
+//
+//  Created by Hardik on 5/7/16.
+//  Copyright © 2016 Pinch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVNProgresslib.
+FOUNDATION_EXPORT double KVNProgresslibVersionNumber;
+
+//! Project version string for KVNProgresslib.
+FOUNDATION_EXPORT const unsigned char KVNProgresslibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVNProgresslib/PublicHeader.h>
+
+
